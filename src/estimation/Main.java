020f6a3620fb407660aa95ac935a309a6534a1a0
alahmm <Meriem.Al.Ahmadi.Hammou@msg.group>
2023-01-19 22:35:29 +0100

    readability is working now!

--- src/estimation/Main.java
+++ src/estimation/Main.java
@@ -12,15 +12,16 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Main {
-    public static void AgeEstimator(double score) {
+    public static long AgeEstimator(double score) {
         double score2 = Math.ceil(score);
         long score3 = (long)score2;
-        System.out.printf("This text should be understood by %d-%d year-olds.%n", score3 + 4, score3 + 5);
+        //System.out.printf("This text should be understood by %d-%d year-olds.%n", score3 + 4, score3 + 5);
+        return score3 + 5;
     }
 
         public static int SyllableCounter(String string) {
             int counter = 0;
-            final String regex = "[^aeiouyAEIOUY]?[aiouyAEIOUY][^aeiouyAEIOUY]|[^aeiouyAEIOUY]?[eE][^aeiouyAEIOUY .!,?]";
+            final String regex = "[^aeiouyAEIOUY]?[aiouyAIOUY][^aeiouyAEIOUY]|[eE][^aeiouyAEIOUY .!,?]|[aeiouyAEIOUY][aeiouyAEIOUY]|[^aeiouyAEIOUY][aiouyAEIOUY]|/\\b(\\w)\\b/";
 
             final Pattern pattern = Pattern.compile(regex, Pattern.MULTILINE);
             final Matcher matcher = pattern.matcher(string);
@@ -37,7 +38,7 @@ public class Main {
         double counter = 0;
         double counterC = 0;
         double counterS = 0;
-        int counterP = 0;
+        double counterP = 0;
         String regex = "[!.?]";
         String regexC = "[^ \\n\\t]";
         for (int i = 0; i < input.length(); i++) {
@@ -57,38 +58,66 @@ public class Main {
             String[] listOfPoly = variable.split(" ");
             for (String var : listOfPoly
                  ) {
-                if (SyllableCounter(var) >= 2) {
+                if (SyllableCounter(var) > 2) {
                     counterP ++;
+                }
                 }
             }
 
-        }
         System.out.printf("Words: %d%n", (int) counter);
         System.out.printf("Sentences: %d%n", list.length);
         System.out.printf("Characters: %d%n", (int) counterC);
-        System.out.printf("Syllables: %d%n",  counterS);
+        System.out.printf("Syllables: %d%n",  (int)counterS);
+        System.out.printf("Polysyllables: %d%n",  (int)counterP);
+        System.out.print("Enter the score you want to calculate (ARI, FK, SMOG, CL, all):");
+        Scanner scanner = new Scanner(System.in);
+        String input2 = scanner.next();
+
 
         double score = 4.71 * counterC / counter + 0.5 * counter / (double) list.length - 21.43;
         score = ((long) (score * 100)) / 100.0; // truncates down to 0
         double scoreFK = 0.39 * counter / (double) list.length + 11.8 * counterS / counter - 15.59;
         scoreFK= ((long) (scoreFK * 100)) / 100.0;
+        double scoreSMOG = 1.043 * Math.sqrt(counterP * 30 / (double) list.length) + 3.1291;
+        scoreSMOG= ((long) (scoreSMOG * 100)) / 100.0;
+        double L = 100 * counterC / counter;
+        double S = (double) list.length * 100 / counter;
+        double scoreCL = 0.0588 * L - 0.296 * S - 15.8;
+        scoreCL= ((long) (scoreCL * 100)) / 100.0;
+        System.out.println();
+        if (input2.equals("ARI")) {
+            System.out.printf("Automated Readability Index: %.2f (about %d-year-olds)%n", score, AgeEstimator(score));
+        } else if (input2.equals("FK")) {
+            System.out.printf("Flesch–Kincaid readability tests: %.2f (about %d-year-olds)%n", scoreFK, AgeEstimator(scoreFK));
+        } else if (input2.equals("SMOG")) {
+            System.out.printf("Simple Measure of Gobbledygook: %.2f (about %d-year-olds)%n", scoreSMOG, AgeEstimator(scoreSMOG));
+        } else if (input2.equals("CL")) {
+            System.out.printf("Coleman–Liau index: %.2f (about %d-year-olds)%n", scoreCL, AgeEstimator(scoreCL));
+        } else {
+            System.out.printf("Automated Readability Index: %.2f (about %d-year-olds)%n", score, AgeEstimator(score));
+            System.out.printf("Flesch–Kincaid readability tests: %.2f (about %d-year-olds)%n", scoreFK, AgeEstimator(scoreFK));
+            System.out.printf("Simple Measure of Gobbledygook: %.2f (about %d-year-olds)%n", scoreSMOG, AgeEstimator(scoreSMOG));
+            System.out.printf("Coleman–Liau index: %.2f (about %d-year-olds)%n", scoreCL, AgeEstimator(scoreCL));
+            System.out.println();
+            double var = (double) (AgeEstimator(score) + AgeEstimator(scoreFK) + AgeEstimator(scoreSMOG) + AgeEstimator(scoreCL))/ 4;
+            System.out.printf("This text should be understood in average by %.2f-year-olds.", var);
+        }
 
-        System.out.printf("The score is: %.2f%n", score);
-        AgeEstimator(score);
+
     }
 
     public static void main(String[] args) throws IOException {
         System.out.println("the text is:");
-/*        Scanner scanner = new Scanner(new File(args[0]));
+        Scanner scanner = new Scanner(new File(args[0]));
         while (scanner.hasNext()) {
 
         String input = scanner.nextLine();
         Estimator(input);
 
         }
-        scanner.close();*/
-        Scanner scanner = new Scanner(System.in);
+        scanner.close();
+/*        Scanner scanner = new Scanner(System.in);
         String input = scanner.nextLine();
-        Estimator(input);
+        Estimator(input);*/
     }
 }
--- src/test/Example.java
+++ src/test/Example.java
@@ -7,13 +7,14 @@ public class Example {
     public static void main(String[] args) {
         int counter = 0;
         final String regex = "[^aeiouyAEIOUY]?[aiouyAEIOUY][^aeiouyAEIOUY]|[^aeiouyAEIOUY]?[eE][^aeiouyAEIOUY .!,?]";
-        final String string = "hallo wikipedia";
+        //[^aeiouyAEIOUY]?[aiouyAIOUY][^aeiouyAEIOUY]|[eE][^aeiouyAEIOUY .!,?]|[aeiouyAEIOUY][aeiouyAEIOUY]
+        final String string = "hallo wikipedia ";
 
         final Pattern pattern = Pattern.compile(regex, Pattern.MULTILINE);
         final Matcher matcher = pattern.matcher(string);
 
         while (matcher.find()) {
-            //System.out.println("Full match: " + matcher.group(0));
+            System.out.println("Full match: " + matcher.group(0));
             //counter ++;
             for (int i = 0; i <= matcher.groupCount(); i++) {
                 System.out.println("Group " + i + ": " + matcher.group(i));
